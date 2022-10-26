546dcd6b5663d0e75f6262be2010c3fb840c13ef
Eoin Gall <g00392003@atu.ie>
2022-10-26 13:09:00 +0100

    Project setup

--- src/main/java/ie/atu/week6/Passenger.java
+++ src/main/java/ie/atu/week6/Passenger.java
@@ -1,4 +1,34 @@
 package ie.atu.week6;
 
 public class Passenger {
+
+    private String userTitle;
+    private String userName;
+    private String userID;
+    private String Phone;
+    private int Age;
+
+    public Passenger(String title, String name, String id, String phone, int age) {
+        userTitle = title;
+        userName = name;
+        userID = id;
+        Phone = phone;
+        Age = age;
+    }
+
+    public Passenger() {
+    }
+
+
+    public String getTitle(String title) {
+        if(title.equals("Mr")||title.equals("Mrs")||title.equals("Ms"))
+        {
+            userTitle = title;
+        }
+        else
+        {
+            throw new IllegalArgumentException("This is not a valid Title");
+        }
+        return userTitle;
+    }
 }
--- src/test/java/ie/atu/week6/PassengerTest.java
+++ src/test/java/ie/atu/week6/PassengerTest.java
@@ -2,13 +2,23 @@ package ie.atu.week6;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
+    Passenger myPass;
 
     @BeforeEach
     void setUp() {
+        myPass = new Passenger();
+    }
+
+    @Test
+    void TestgetTitle(){
+        assertEquals("Mr",myPass.getTitle("Mr"));
+        assertEquals("Mrs",myPass.getTitle("Mrs"));
+        assertEquals("Ms",myPass.getTitle("Ms"));
     }
 
     @AfterEach
